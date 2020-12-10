9de5e6ca25eeb6d7f169706f19fe30dd4bb9c898
Garett Roberts <mygarett@gmail.com>
2020-12-09 19:06:49 -0800

    Finished day 1! Both parts (a lot of build issues and such, but got it figured out)

---
+++ AdventOfCode2020/Day1.h
@@ -0,0 +1,91 @@
+#ifndef _DAY1_H_
+#define _DAY1_H_
+
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+#include <folly/FBString.h>
+#include <folly/FBVector.h>
+
+namespace AdventOfCode2020
+{
+
+	class Day1
+	{
+	private:
+		folly::fbstring input;
+
+		const std::vector<int> loadData()
+		{
+			int entry;
+			std::vector<int> out;
+			std::ifstream infile(input.toStdString());
+
+			while (infile >> entry)
+			{
+				out.emplace_back(entry);
+			}
+
+			return out;
+		}
+
+	public:
+		Day1() 
+			: input("Day1Source.txt")
+		{}
+
+		const int solutionp1()
+		{
+			int entry = -1;
+			std::unordered_map<int, int> additionSet;
+			std::ifstream infile(input.toStdString());
+
+			while (infile >> entry)
+			{
+				const auto found = additionSet.find(entry);
+				if (found != additionSet.end())
+				{
+					return entry * found->second;
+				}
+
+				int remainder = 2020 - entry;
+				additionSet.insert_or_assign(remainder, entry);
+			}
+
+			return -1;
+		}
+
+		const int solutionp2()
+		{
+			int desiredSum = 2020;
+
+			std::vector<int> data = loadData(); // cant use fbvector... can't sort
+			std::sort(data.begin(), data.end());
+
+			int dataSize = static_cast<int>(data.size());
+
+			for(int i = 0; i < dataSize - 2; i++)
+			{
+				int j = i + 1;
+				int k = dataSize - 1;
+
+				while (j < k)
+				{ 
+					int sum = data[i] + data[j] + data[k];
+					if (sum == desiredSum)
+						return data[i] * data[j] * data[k];
+					else if (sum > desiredSum)
+						k--;
+					else if (sum < desiredSum)
+						j++;
+				}
+			}
+
+			return -1;
+		}
+	}; // End Day1 Class
+
+} //End AdventOfCode2020 Namespace
+
+#endif
--- AdventOfCode2020/Main.cxx
+++ AdventOfCode2020/Main.cxx
@@ -1,19 +1,13 @@
-// AdventOfCode2020.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "./Day1.h"
 
 #include <iostream>
+#include <memory>
 
 int main()
 {
+	std::unique_ptr<AdventOfCode2020::Day1> d1(new AdventOfCode2020::Day1);
+	const int d1p1Sol(d1->solutionp1());
+	std::cout << "Day Answer 1 Part 1: " << d1p1Sol << std::endl;
+	const int d1p2Sol(d1->solutionp2());
+	std::cout << "Day Answer 1 Part 2: " << d1p2Sol << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
