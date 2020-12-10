1d26b24647c10284b6e2574fd2dc3f135fedaaa2
Garett Roberts <mygarett@gmail.com>
2020-12-10 09:35:41 -0800

    Setup Day2 for parsing the data

--- AdventOfCode2020/Day2/Day2.h
+++ AdventOfCode2020/Day2/Day2.h
@@ -1,23 +1,85 @@
 #ifndef _DAY2_H_
 #define _DAY2_H_
 
+#include "../Helper/InputLoader.h"
+
+#include <iostream>
+#include <istream>
+
 #include <folly/FBString.h>
 #include <folly/FBVector.h>
 
 namespace AdventOfCode2020
 {
 
+	class PasswordRequirement
+	{
+	private:
+		folly::fbstring password;
+		int min;
+		int max;
+		char desiredChar;
+
+	public:
+		//No real error handling here.... Just for brevity
+		friend std::istream& operator>>(std::istream& input, PasswordRequirement& passwordRequirement)
+		{
+			std::string line;
+			std::getline(input, line);
+
+			if (line == "")
+				return input;
+
+			//Left Half Processing
+			size_t firstHalfLocation = line.find(":");
+			folly::fbstring requirements = line.substr(0, firstHalfLocation);
+
+			//Min-Max Processing
+			size_t splitLocation = requirements.find(" ");
+			folly::fbstring minMax = requirements.substr(0, splitLocation);
+			passwordRequirement.min = std::stoi(minMax.substr(0).toStdString(), nullptr);
+			passwordRequirement.max = std::stoi(minMax.substr(requirements.find("-") + 1).toStdString(), nullptr);
+
+			//Desired Char Processing
+			passwordRequirement.desiredChar = requirements.at(firstHalfLocation - 1);
+
+			//Password Processing
+			passwordRequirement.password = line.substr(firstHalfLocation + 2, line.size());
+
+			return input;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const PasswordRequirement& passwordRequirement)
+		{
+			os << "-- Password Requirement --" << std::endl;
+			os << " -Password: " << passwordRequirement.password << std::endl;
+			os << " -DesiredChar: " << passwordRequirement.desiredChar << std::endl;
+			os << " -Min: " << passwordRequirement.min << std::endl;
+			os << " -Max: " << passwordRequirement.max << std::endl;
+			os << "--------------------------" << std::endl;
+
+			return os;
+		}
+	};
+
 	class Day2
 	{
 	private:
 		folly::fbstring input;
+		folly::fbvector<PasswordRequirement> data;
 
 	public:
 		Day2()
-			: input("Day2Source.txt")
-		{}
+			: input("./Day2/Day2Source.txt")
+		{
+			data = AdventOfCode2020::InputLoader().getInput<PasswordRequirement>(input);
+		}
 
-		int part1() { return -1; };
+		int part1() 
+		{ 
+			return -1;
+		};
+
 		int part2() { return -1; };
 	}; //End of class Day2
 } //End of AdventOfCode2020 namespace
--- AdventOfCode2020/Main.cxx
+++ AdventOfCode2020/Main.cxx
@@ -1,13 +1,23 @@
 #include "./Day1/Day1.h"
+#include "./Day2/Day2.h"
 
 #include <iostream>
 #include <memory>
 
 int main()
 {
+	std::cout << "---- DAY 1 ----" << std::endl;
 	std::unique_ptr<AdventOfCode2020::Day1> d1(new AdventOfCode2020::Day1);
-	const int d1p1Sol(d1->solutionp1());
-	std::cout << "Day Answer 1 Part 1: " << d1p1Sol << std::endl;
-	const int d1p2Sol(d1->solutionp2());
-	std::cout << "Day Answer 1 Part 2: " << d1p2Sol << std::endl;
+	const int d1p1(d1->solutionp1());
+	std::cout << "Part 1: " << d1p1 << std::endl;
+	const int d1p2(d1->solutionp2());
+	std::cout << "Part 2: " << d1p2 << std::endl;
+
+	std::cout << "---- DAY 2 ----" << std::endl;
+	std::unique_ptr<AdventOfCode2020::Day2> d2(new AdventOfCode2020::Day2);
+	const int d2p1(d2->part1());
+	std::cout << "Part 1: " << d2p1 << std::endl;
+	const int d2p2(d2->part2());
+	std::cout << "Part 2: " << d2p2 << std::endl;
+
 }
